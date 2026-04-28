583068027d786ba7ef70bf81baa5cb3b61d32a28
kavyan256 <iit2024245@iiita.ac.in>
2026-04-28 10:00:47 +0530

    consolidate the inputs files

--- client/input/input.c
+++ client/input/input.c
@@ -4,7 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../core/game.h"
+
 static PlayerInput g_input;
+static unsigned char g_keys[256];
 static volatile int g_ready_toggle_requested = 0;
 static volatile int g_character_prev_requested = 0;
 static volatile int g_character_next_requested = 0;
@@ -12,22 +15,30 @@ static volatile int g_character_next_requested = 0;
 static void on_key_down(unsigned char key, int x, int y) {
     (void)x;
     (void)y;
+    g_keys[key] = 1;
     
     switch (key) {
         case 'w': case 'W': g_input.forward = 1; break;
         case 's': case 'S': g_input.backward = 1; break;
         case 'a': case 'A': g_input.strafe_left = 1; break;
         case 'd': case 'D': g_input.strafe_right = 1; break;
-        case 'r': case 'R': g_ready_toggle_requested = 1; break;
-        case 'q': case 'Q': g_character_prev_requested = 1; break;
-        case 'e': case 'E': g_character_next_requested = 1; break;
         case 27: /* ESC */ exit(0); break;
+    }
+    
+    /* Lobby-only hotkeys */
+    if (!game_has_started()) {
+        switch (key) {
+            case 'r': case 'R': g_ready_toggle_requested = 1; break;
+            case 'q': case 'Q': g_character_prev_requested = 1; break;
+            case 'e': case 'E': g_character_next_requested = 1; break;
+        }
     }
 }
 
 static void on_key_up(unsigned char key, int x, int y) {
     (void)x;
     (void)y;
+    g_keys[key] = 0;
     
     switch (key) {
         case 'w': case 'W': g_input.forward = 0; break;
@@ -85,6 +96,7 @@ int input_consume_character_next(void) {
 
 void input_init_callbacks(void) {
     memset(&g_input, 0, sizeof(g_input));
+    memset(g_keys, 0, sizeof(g_keys));
     
     glutKeyboardFunc(on_key_down);
     glutKeyboardUpFunc(on_key_up);
